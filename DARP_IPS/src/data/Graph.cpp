3d936587584494cfcf4aec9396460477572b9b43
Elahe Amiri <elahe.amiri@polymtl.ca>
2021-10-16 22:23:35 -0400

    define function for adding sink and source,  define node status enum

--- DARP_IPS/src/data/Graph.cpp
+++ DARP_IPS/src/data/Graph.cpp
@@ -15,6 +15,9 @@ using std::to_string;
 Node::Node(string nodeId, PRequest &relatedRequest, NodeType type) : nodeID_(nodeId),
                                                                 related_Request_(&relatedRequest), type_(type) {
     reachTime_ = 0;
+    deltaTime_ = relatedRequest->deltaTime_;
+    nodeStatus_ = DEFINED;
+
     if (type == PICKUP){
         locLatitude_ = relatedRequest->PickUpLatitude_;
         locLongitude_ = relatedRequest->PickUpLongitude_;
@@ -25,8 +28,21 @@ Node::Node(string nodeId, PRequest &relatedRequest, NodeType type) : nodeID_(nod
         locLongitude_ = relatedRequest->DropOffLongitude_;
         nbPassengers_ = (-1) * relatedRequest->nbPassengers_;
     }
-    deltaTime_ = relatedRequest->deltaTime_;
-    nodeStatus_ = 0;
+}
+
+Node::Node(float locLatitude, float locLongitude, NodeType type) : locLatitude_(locLatitude),
+                                                                   locLongitude_(locLongitude),
+                                                                   type_(type) {
+    related_Request_ = nullptr;
+    reachTime_ = 0;
+    nbPassengers_ = 0;
+    deltaTime_ = 0;
+    nodeStatus_ = DEFINED;
+
+    if (type == SOURCE)
+        nodeID_ = "SO_0";
+    else if (type == SINK)
+        nodeID_ = "SI_0";
 }
 
 Node::~Node() = default;
@@ -37,18 +53,30 @@ Node::~Node() = default;
 //-----------------------------------------------------------------------------
 
 // Constructor and Destructor
-Graph::Graph() = default;
+Graph::Graph() {
+    nbNodes_ = 0;
+};
 
+Graph::Graph(PNode source, PNode sink) {
+    nbNodes_ = 2;
+    nodes_.insert(std::pair<std::string, PNode> (source->nodeID_, source));
+    nodes_.insert(std::pair<std::string, PNode> (sink->nodeID_, sink));
+}
+
+// function for updating the graph and adding new request
 void Graph::addNewRequests(std::vector<PRequest> &newRequests) {
     string ID;
     for (int r = 0; r < newRequests.size(); ++r) {
         // create pickup node
-        ID = "Pi_" + to_string(newRequests[r]->requestID_);
+        ID = "PI_" + to_string(newRequests[r]->requestID_);
         nodes_.insert(std::pair<std::string,PNode> (ID, std::make_shared<Node>(ID, newRequests[r], PICKUP)));
 
         // create drop off node
-        ID = "Dr_" + to_string(newRequests[r]->requestID_);
+        ID = "DR_" + to_string(newRequests[r]->requestID_);
         nodes_.insert(std::pair<std::string,PNode> (ID, std::make_shared<Node>(ID, newRequests[r], DROPOFF)));
+        nbNodes_ = nbNodes_ + 2;
     }
 }
+
+
 
--- DARP_IPS/src/data/Graph.h
+++ DARP_IPS/src/data/Graph.h
@@ -7,7 +7,6 @@
 
 #include "utilities/MyTools.h"
 #include "data/Instance.h"
-#include "Request.h"
 
 
 //-----------------------------------------------------------------------------
@@ -16,26 +15,26 @@
 //-----------------------------------------------------------------------------
 
 // Different node types and their names
-enum NodeType {
-    SOURCE, SINK, PICKUP, DROPOFF
-};
+enum NodeType { SOURCE, SINK, PICKUP, DROPOFF };
 static const std::vector<std::string> nodeTypeName = {
         "SOURCE_NODE ", "SINK_NODE   ", "PICKUP_NODE ", "DROPOFF_NODE"
 };
 
-// useful types
-class Node;
-typedef std::shared_ptr<Node> PNode;
+// Different node status and their names
+enum NodeStatus { DEFINED = 0, PLANNED = 1, DONE = 2 };
+static const std::vector<std::string> nodeStatusName = {
+        "NO_ACTION", "PLANNED  ", "COMPLETED"
+};
 
 // Defining the properties of the graph nodes
 class Node {
 public:
-    string nodeID_;              // node ID
-    PRequest* related_Request_;      // pointer to its request
-//    Node* pairNode_;                // related pickup/  drop off
+    string nodeID_;                 // node ID
+    PRequest* related_Request_;     // pointer to its request
+//    Node* pairNode_;              // related pickup/  drop off
     float locLatitude_;             // node location latitude
     float locLongitude_;            // node location longitude
-    NodeType type_;                // node type: 0-pick up , 1-drop off, 3-source, 4-sink
+    NodeType type_;                 // node type: pick up, drop off, source, sink
     float reachTime_;               // the time that vehicle reach to the node
     int nbPassengers_;              // number of passengers to pick up or drop off
     float deltaTime_;               // time to perform pick up or drop off
@@ -43,27 +42,28 @@ public:
 
     // Constructor and Destructor
     Node(string nodeId, PRequest &relatedRequest, NodeType type);
+    Node(float locLatitude, float locLongitude, NodeType type);
 
     virtual ~Node();
-
 };
+typedef std::shared_ptr<Node> PNode;
 
 //-----------------------------------------------------------------------------
 //  Graph class
 //-----------------------------------------------------------------------------
 
-class Graph;
-typedef std::shared_ptr<Graph> PGraph;
-
 class Graph {
 public:
+    int nbNodes_;
     std::map<std::string,PNode> nodes_;
 
     // Constructor and Destructor
     Graph();
+    Graph(PNode source, PNode sink);
 
+    // function for updating the graph and adding new request
     void addNewRequests(std::vector<PRequest> &newRequests);
 };
-
+typedef std::shared_ptr<Graph> PGraph;
 
 #endif //_GRAPH_H
