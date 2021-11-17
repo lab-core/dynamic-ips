4fd5740a0340af030e4297944569a580a0f49178
Elahe Amiri <elahe.amiri@polymtl.ca>
2021-11-17 10:10:37 -0500

    add name item to the Request class

--- DARP_IPS/src/data/Request.cpp
+++ DARP_IPS/src/data/Request.cpp
@@ -10,26 +10,44 @@
 //  contains the request info request time, number of passengers, pickup and drop off location
 //---------------------------------------------------------------------------------------------
 
+unsigned int Request::requestCount_ = 0;
+
 // Constructor and Destructor
-Request::Request(const int requestId, float pickUpLatitude, float pickUpLongitude, float dropOffLatitude,
+Request::Request(float pickUpLatitude, float pickUpLongitude, float dropOffLatitude,
                  float dropOffLongitude, float earlyPick, int nbPassengers, float deltaTime, float minDist,
-                 float minTravelTime) : requestID_(requestId), PickUpLatitude_(pickUpLatitude),
+                 float minTravelTime) : requestID_(requestCount_++), PickUpLatitude_(pickUpLatitude),
                  PickUpLongitude_(pickUpLongitude), DropOffLatitude_(dropOffLatitude),
                  DropOffLongitude_(dropOffLongitude), earlyPick_(earlyPick), nbPassengers_(nbPassengers),
                  deltaTime_(deltaTime), minDist_(minDist), minTravelTime_(minTravelTime) {
 
     requestStatus_ = NO_ACTION;
     penalty_ = 0;
+    char* name2 = new char[255];
+    strncpy(name2, std::to_string(requestID_).c_str(), 255);
+    name_ = name2;
 }
 
 Request::~Request() {}
 
+// Getters and Setters
+void Request::setPenalty(int epoch) {
+    penalty_ = deltaPram * pow(2, ((epochLength * epoch) - earlyPick_) / (10 * epochLength));
+
+}
+const unsigned int Request::getRequestId() const {return requestID_;}
+
 // Display function
 std::string Request::toString() const {
     std::stringstream repStr;
-    repStr << "\t" << "# REQUEST ( " << requestID_ << " ) :" << std::endl;
-    repStr << "\t" << "# PICKUP_COORDINATE    :" << "(" << PickUpLatitude_ << " , " << PickUpLongitude_ << ")" << std::endl;
-    repStr << "\t" << "# DROPOFF_COORDINATE   :" << "(" << DropOffLatitude_ << " , " << DropOffLongitude_ << ")" << std::endl;
-    repStr << "\t" << "# NUMBER_OF_PASSENGERS :" << nbPassengers_ << std::endl;
+    repStr << std::left;
+    repStr << "# REQUEST ( " << requestID_ << " )" << std::endl;
+    repStr << "#\t" << std::setw(24) << "- PICKUP_COORDINATE" << " : " << "(" << PickUpLatitude_ << " , " << PickUpLongitude_ << ")" << std::endl;
+    repStr << "#\t" << std::setw(24) << "- DROPOFF_COORDINATE" << " : " << "(" << DropOffLatitude_ << " , " << DropOffLongitude_ << ")" << std::endl;
+    repStr << "#\t" << std::setw(24) << "- NUMBER_OF_PASSENGERS" << " : " << nbPassengers_ << std::endl;
+    repStr << "#" << std::endl;
     return repStr.str();
 }
+
+
+
+
--- DARP_IPS/src/data/Request.h
+++ DARP_IPS/src/data/Request.h
@@ -16,32 +16,42 @@ enum RequestStatus {NO_ACTION = 0, ON_BOARD = 1, COMPLETED = 2};
 static const std::vector<std::string> reqStatusName = {
         "NO_ACTION", "ON_BOARD ", "COMPLETED" };
 
+
+
 class Request {
+private:
+    const unsigned int requestID_;      // request ID
 public:
-    const int requestID_;           // request ID
-    float PickUpLatitude_;          // pick up location latitude
-    float PickUpLongitude_;         // pick up location longitude
-    float DropOffLatitude_;         // Drop off location latitude
-    float DropOffLongitude_;        // Drop off location longitude
-    float earlyPick_;               // earliest possible pick up time for the request
-    int nbPassengers_;              // number of passengers to pick up or drop off
-    float deltaTime_;               // time to perform pick up or drop off
-    float minDist_;                 // minimum travel distance between pickup and drop off location
-    float minTravelTime_;           // minimum travel time between pickup and drop off location
-    float penalty_;                 // penalty of not serving at current period
-    RequestStatus requestStatus_;          // status of the request 0:no action 1:picked up 2:complete
+    static unsigned int requestCount_;  // Counter the number of requests
+    const char* name_;
+    float PickUpLatitude_;              // pick up location latitude
+    float PickUpLongitude_;             // pick up location longitude
+    float DropOffLatitude_;             // Drop off location latitude
+    float DropOffLongitude_;            // Drop off location longitude
+    float earlyPick_;                   // earliest possible pick up time for the request
+    int nbPassengers_;                  // number of passengers to pick up or drop off
+    float deltaTime_;                   // time to perform pick up or drop off
+    float minDist_;                     // minimum travel distance between pickup and drop off location
+    float minTravelTime_;               // minimum travel time between pickup and drop off location
+    float penalty_;                     // penalty of not serving at current period
+    RequestStatus requestStatus_;       // status of the request 0:no action 1:on board 2:complete
 
     // Constructor and Destructor
-    Request(const int requestId, float pickUpLatitude, float pickUpLongitude, float dropOffLatitude,
+    Request(float pickUpLatitude, float pickUpLongitude, float dropOffLatitude,
             float dropOffLongitude, float earlyPick, int nbPassengers, float deltaTime, float minDist,
             float minTravelTime);
 
     virtual ~Request();
 
+    // Getters and Setters
+    void setPenalty(int epoch);
+
+    const unsigned int getRequestId() const;
+
     // Display function
     std::string toString() const;
 };
-typedef std::shared_ptr<Request> PRequest;
+
 
 
 #endif //_REQUEST_H
