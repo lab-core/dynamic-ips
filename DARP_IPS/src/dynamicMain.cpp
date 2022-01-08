1b99b85af3788d971bf5f4a3bf78e723c814bbd1
Elahe Amiri <elahe.amiri@polymtl.ca>
2022-01-08 09:47:26 -0500

    create distance Matrix from OSRM data in the start

--- DARP_IPS/src/dynamicMain.cpp
+++ DARP_IPS/src/dynamicMain.cpp
@@ -1,9 +1,9 @@
 //
 // Created by Ella on 2021-11-28.
 //
+#define CURL_STATICLIB
 
 #include "utilities/ReadWrite.h"
-#include <iostream>
 #include "solvers/ISUDAlgorithm.h"
 #include "solvers/SubProblem.h"
 #include "data/Instance.h"
@@ -12,15 +12,22 @@
 using std::string;
 using namespace std::chrono;
 
+PTravelTime travelMat;
+
+
 int main() {
 
+
+
     int nbReceivedRequest = 0;
-    int epoch = 0;
+    int epoch = 1;
 
     Tools::Timer *subProTime = new Tools::Timer(); subProTime->init();;
 
     string dataDir = "datasets/";
     string instanceName = "20150713_16-03m";
+
+
 
     // build the path of input files
     InputPaths inputPaths(dataDir, instanceName);
@@ -29,24 +36,37 @@ int main() {
     std::cout << "# INITIALIZE OF THE MAIN INSTANCE" << std::endl;
     PInstance mainInst = ReadWrite::readInstance(inputPaths.getInstanceData());
     ReadWrite::readTripRequests(inputPaths.getTripData(), mainInst);
-    // updating vehicle data and setting their current route to an empty route
+
+    // creating distance matrix based on the data from OSRM server
+    travelMat = std::make_shared<TravelTime>();
+    travelMat->setNodeIdToInt(mainInst->instGraph_->nodeIDToInt_);
+    travelMat->setDurationMat(mainInst->instGraph_);
+
+    // setting min travel times of requests based on the distance matrix
+    // initializing vehicles empty route and current route
+    mainInst->updateMinTravelTimes();
+    for (int v = 0; v < mainInst->nbVehicles_; ++v) {
+        mainInst->vehicles_[v]->setEmptyRoute(mainInst);
+        mainInst->vehicles_[v]->setCurrentRoute(mainInst->vehicles_[v]->emptyRoute_);
+    }
+
 
     std::cout << std::endl;
     std::cout << mainInst->toString();
 
-    std::shared_ptr<ISUDAlgorithm> isudObj = std::make_shared<ISUDAlgorithm>();
 
+    std::shared_ptr<ISUDAlgorithm> isudObj = std::make_shared<ISUDAlgorithm>();
+//    MIPSolver(mainInst);
     double previousObj = 0;
     while (nbReceivedRequest < mainInst->nbRequests_) {
-        std::cout << "************************************************************************************" << std::endl;
-        std::cout << " *****************************  epoch " << epoch << "  *****************************" << std::endl;
-        std::cout << "************************************************************************************" << std::endl;
+        std::cout << " *****************************  epoch " << std::setw(3) << epoch << "  *****************************" << std::endl;
         subProTime->start();
         for (int v = 0; v < mainInst->nbVehicles_; ++v) {
             mainInst->vehicles_[v]->updateStatus(epoch);
         }
         PInstance EpochInst = std::make_shared<Instance>(*mainInst);
-        EpochInst->buildPartialData(mainInst, isudObj->zSolution_ , epoch, nbReceivedRequest);
+        // reading the data received in previous epoch (epoch -1)
+        EpochInst->buildPartialData(mainInst, isudObj->zSolution_ , epoch-1, nbReceivedRequest);
         nbReceivedRequest += EpochInst->nbNewRequests_;
         subProTime->stop();
         isudObj->initialization(EpochInst);
@@ -55,8 +75,10 @@ int main() {
 
         // solving the reduced problem at the start of each epoch
         int flagImprove = 0;
+        /*sort(mainInst->vehicles_.begin(),mainInst->vehicles_.end(),[](const PVehicle &lhs, const PVehicle &rhs){
+            return lhs->currentRoute_->routeSize_ > rhs->currentRoute_->routeSize_;});*/
         sort(mainInst->vehicles_.begin(),mainInst->vehicles_.end(),[](const PVehicle &lhs, const PVehicle &rhs){
-            return lhs->currentRoute_->routeSize_ > rhs->currentRoute_->routeSize_;});
+            return lhs->dual_ > rhs->dual_;});
 
         while (flagImprove == 0) {
             subProTime->start();
@@ -102,7 +124,7 @@ int main() {
         for (int r = 0; r < isudObj->routeSolution_.size(); ++r) {
             EpochInst->vehicles_[isudObj->routeSolution_[r]->vehicleID_]->setCurrentRoute(isudObj->routeSolution_[r]);
         }
-        std::cout << "# Solution after ISUD:" << std::endl;
+        std::cout << "# Solution of ISUD after epoch " << epoch << " : " << std::endl;
         std::cout << isudObj->toString();
         epoch++;
 
@@ -113,9 +135,22 @@ int main() {
     for (int v = 0; v < mainInst->nbVehicles_; ++v) {
         if (mainInst->vehicles_[v]->solutionRoute_->routeNodes_.back()->type_ == SOURCE) {
             for (int i = 1; i < mainInst->vehicles_[v]->currentRoute_->routeSize_; ++i) {
+                mainInst->vehicles_[v]->currentRoute_->routeNodes_[i]->nodeStatus_ = DONE;
+                mainInst->vehicles_[v]->currentRoute_->routeNodes_[i]->reachTime_ =
+                        mainInst->vehicles_[v]->currentRoute_->plannedReachTime_[i];
+                if (i != mainInst->vehicles_[v]->currentRoute_->routeSize_ - 1)
+                    (*mainInst->vehicles_[v]->currentRoute_->routeNodes_[i]->related_Request_)->requestStatus_ = COMPLETED;
                 mainInst->vehicles_[v]->solutionRoute_->addNode(mainInst->vehicles_[v]->currentRoute_->routeNodes_[i],
                                                                 mainInst->vehicles_[v]->currentRoute_->plannedReachTime_[i],
                                                                 mainInst->vehicles_[v]->currentRoute_->plannedPassengers_[i]);
+
+                if (mainInst->vehicles_[v]->currentRoute_->routeNodes_[i]->type_ == PICKUP) {
+                    (*mainInst->vehicles_[v]->currentRoute_->routeNodes_[i]->related_Request_)->pickTime_ =
+                            mainInst->vehicles_[v]->currentRoute_->plannedReachTime_[i];
+                }
+                else if (mainInst->vehicles_[v]->currentRoute_->routeNodes_[i]->type_ == DROPOFF)
+                    (*mainInst->vehicles_[v]->currentRoute_->routeNodes_[i]->related_Request_)->dropTime_ =
+                            mainInst->vehicles_[v]->currentRoute_->plannedReachTime_[i];
             }
         }
         else
@@ -124,18 +159,34 @@ int main() {
                                                             mainInst->vehicles_[v]->currentRoute_->plannedPassengers_.back());
 
     }
+    std::cout << std::endl << std::endl;
 
-    int numServed = 0;
-    double totalDelay = 0;
-    std::cout << "# Final solution of the routes after " << epoch << " epochs: " << std::endl;
+    std::string logfilePath = dataDir + instanceName + "/LogFile_" + instanceName + ".txt";
+    freopen (logfilePath.c_str(),"w",stdout);
+
+
+    std::cout << "*************************************************************************************" << std::endl;
+    std::cout << "*********************** FINAL VEHICLE ROUTES AFTER " << std::setw(3) << epoch << " EPOCHS *********************** " << std::endl;
+    std::cout << "*************************************************************************************" << std::endl;
+    std::cout << std::endl << std::endl;
+
     for (int v = 0; v < mainInst->nbVehicles_; ++v) {
         std::cout << mainInst->vehicles_[v]->solutionRoute_->toString();
-        totalDelay += mainInst->vehicles_[v]->solutionRoute_->totalDelay_;
-        numServed += mainInst->vehicles_[v]->solutionRoute_->routeRequests.size();
     }
-    std::cout << "# Time spent on ISUD improvement    = " << isudObj->isudTime_->dSinceInit().count() << " (seconds)" << std::endl;
-    std::cout << "# Time spent on solving subproblems = " << subProTime->dSinceInit().count() << " (seconds)" << std::endl;
-    std::cout << "# Total delay                       = " << totalDelay << std::endl;
-    std::cout << "# Number of served requests       = " << numServed << std::endl;
-    std::cout << "# Total number of requests          = " << mainInst->nbRequests_ << std::endl;
+    std::cout << "#" << std::endl;
+
+
+
+    std::cout << mainInst->solutionToString();
+    std::cout << std::left << std::fixed << std::setprecision(2);
+    std::cout << std::setw(37) << "# Time spent on ISUD improvement" << " = " << isudObj->isudTime_->dSinceInit().count() << " (s)" << std::endl;
+    std::cout << std::setw(37) << "# Time spent on solving subproblems" << " = " << subProTime->dSinceInit().count() << " (s)" << std::endl;
+
+    string outputRouteDir = dataDir + instanceName + "/Routes_" + instanceName + ".csv";
+    string outputRequestDir = dataDir + instanceName + "/Requests_" + instanceName + ".csv";
+    mainInst->saveSolutionRoutes(outputRouteDir);
+    mainInst->saveRequestsResults(outputRequestDir);
+
+    fclose (stdout);
+
 }
