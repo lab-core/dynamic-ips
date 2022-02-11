16b1357e2a0266ad5ab160b9577121b35c57e93b
Elahe Amiri <elahe.amiri@polymtl.ca>
2022-02-11 17:18:13 -0500

    add DurationData to read durations from txt file

--- DARP_IPS/src/utilities/InputPaths.cpp
+++ DARP_IPS/src/utilities/InputPaths.cpp
@@ -13,6 +13,7 @@ InputPaths::InputPaths() {
     tripData_ = "";
     instanceData_ = "";
     instanceName_ = "";
+    durationData_ = "";
 }
 
 InputPaths::InputPaths(std::string datadir, std::string instanceName, double timeOUt)
@@ -21,14 +22,16 @@ InputPaths::InputPaths(std::string datadir, std::string instanceName, double tim
     std::string instanceDir = datadir + instanceName + "/";
 
     //initialize the file names for trip records and instance data
-    tripData_ = instanceDir + "TR_" + instanceName + ".txt";
-    instanceData_ = instanceDir + "IN_" + instanceName + ".txt";
+    tripData_ = instanceDir + "TRIP_" + instanceName + ".txt";
+    instanceData_ = instanceDir + "INSTANCE_" + instanceName + ".txt";
+    durationData_ = instanceDir + "DURATION_" + instanceName + ".txt";
 }
 
 // getters
 const std::string &InputPaths::getInstanceName() const {return instanceName_; }
 const std::string &InputPaths::getTripData() const {return tripData_;}
 const std::string &InputPaths::getInstanceData() const {return instanceData_; }
+const std::string &InputPaths::getDurationData() const {return durationData_; }
 double InputPaths::getTimeOUt() const {return timeOUt; }
 
 // setters
@@ -47,5 +50,7 @@ void InputPaths::setInstanceData(const std::string &instanceData) {
 void InputPaths::setTimeOUt(double timeOUt) {
     InputPaths::timeOUt = timeOUt;
 }
+
+
 
 
