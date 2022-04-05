e15dcd49499a561bb014f85abb096ad192bbe046
Elahe Amiri <elahe.amiri@polymtl.ca>
2022-04-04 22:22:36 -0400

    debug

--- DARP_IPS/src/solvers/ReducedProblem.cpp
+++ DARP_IPS/src/solvers/ReducedProblem.cpp
@@ -167,10 +167,10 @@ void ReducedProblem::solveModel(PInstance &pInst, std::vector<PRequest> &zSoluti
 //        env_.out() << zVal << std::endl;
 
         for (int r = routeVal.getSize()-1; r >= 0; --r) {
-            if (routeVal[r] > 0)
-                std::cout << routeVal[r] << std::endl;
+            /*if (routeVal[r] > 0)
+                std::cout << routeVal[r] << std::endl;*/
 
-            if (routeVal[r] > 0.9) {
+            if (routeVal[r] > 0.1) {
                 routeSolution.push_back(generatedRoutes[routeVar_[r].getName()]);
                 pInst->vehicles_[generatedRoutes[routeVar_[r].getName()]->vehicleID_]->setCurrentRoute(generatedRoutes[routeVar_[r].getName()]);
             }
@@ -179,8 +179,10 @@ void ReducedProblem::solveModel(PInstance &pInst, std::vector<PRequest> &zSoluti
                 routeVar_.remove(r,1);
             }
         }
-        std::cout << "------------" << std::endl;
+//        std::cout << "------------" << std::endl;
         for (int i = zVal.getSize()-1; i >= 0; --i) {
+            /*if (zVal[i] > 0)
+                std::cout << zVal[i] << std::endl;*/
             if (zVal[i] > 0.9)
                 zSolution.push_back(pInst->nameToRequest_[zVar_[i].getName()]);
             else {
