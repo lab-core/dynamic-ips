bb0eeb26e73ad5c83d539658f44a64e540db0f33
Elahe Amiri <elahe.amiri@polymtl.ca>
2022-05-05 21:55:52 -0400

    define greedy algorithm

---
+++ DARP_IPS/src/solvers/GreedySolver.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Ella on 5/5/2022.
+//
+
+#include "GreedySolver.h"
+
+void GreedySolver(PInstance &PInst) {
+    std::vector<float> possibleDelay;
+    for (auto & requestObj : PInst->requests_) {
+        if (requestObj->requestStatus_ == NO_ACTION) {
+            possibleDelay.clear();
+            std::string pickID = Tools::createNodeID(requestObj->getRequestId(), PICKUP);
+            std::string dropID = Tools::createNodeID(requestObj->getRequestId(), DROPOFF);
+
+            for (auto &vehicleObj: PInst->vehicles_) {
+                float requsetPickTime = vehicleObj->currentRoute_->plannedReachTime_.back() +
+                                        vehicleObj->currentRoute_->routeNodes_.back()->deltaTime_ +
+                                        durationMatrix_[vehicleObj->currentRoute_->routeNodes_.back()->locationID_][PInst->instGraph_->nodes_[pickID]->locationID_];
+                possibleDelay.push_back(requsetPickTime);
+            }
+            int vehicle_ID = std::min_element(possibleDelay.begin(), possibleDelay.end()) - possibleDelay.begin();
+            PInst->vehicles_[vehicle_ID]->currentRoute_->addNode(PInst->instGraph_->nodes_[pickID]);
+            PInst->vehicles_[vehicle_ID]->currentRoute_->addNode(PInst->instGraph_->nodes_[dropID]);
+        }
+    }
+}
---
+++ DARP_IPS/src/solvers/GreedySolver.h
@@ -0,0 +1,18 @@
+//
+// Created by Ella on 5/5/2022.
+//
+
+#ifndef _GREEDYSOLVER_H
+#define _GREEDYSOLVER_H
+
+#include "data/Instance.h"
+#include "utilities/InputPaths.h"
+
+//-----------------------------------------------------------------------------
+//  Contains Greedy function to solve the instance
+//  Requests are assigned to the vehicles based on the minimum possible pickup
+//-----------------------------------------------------------------------------
+void GreedySolver(PInstance& PInst);
+
+
+#endif //_GREEDYSOLVER_H
