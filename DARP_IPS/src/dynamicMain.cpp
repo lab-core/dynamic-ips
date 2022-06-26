e6bdb1996384bf4cc05985e2a840fbb0d92124c5
Elahe Amiri <elahe.amiri@polymtl.ca>
2022-06-26 18:39:59 -0400

    test

--- DARP_IPS/src/dynamicMain.cpp
+++ DARP_IPS/src/dynamicMain.cpp
@@ -198,9 +198,9 @@ int main() {
                         }
                         else if (mainInst->parameters_->mainAlgorithm_ == CG_ISUD){
                             isudObj->solveISUD(EpochInst, epoch, inputPaths.getOutputEpochIsud());
-                            /*std::cout << "# SOLUTION ROUTES AFTER SOLVING ISUD FOR EPOCH " << epoch << ":" << std::endl;
+                            std::cout << "# SOLUTION ROUTES AFTER SOLVING ISUD FOR EPOCH " << epoch << ":" << std::endl;
                             for (auto &routeObj: isudObj->routeSolution_)
-                                std::cout << routeObj->toString();*/
+                                std::cout << routeObj->toString();
                         }
                     }
                     if (previousObj == isudObj->objValue_) {
