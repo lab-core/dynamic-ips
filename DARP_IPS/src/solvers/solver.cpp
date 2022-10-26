f7dba40b756707e5fa993c0dcff4c92f577e1946
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2022-10-26 14:20:50 -0400

    define solver class for dynamic and static mode

---
+++ DARP_IPS/src/solvers/solver.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Elahe Amiri on 2022-10-26.
+//
+
+#include "solver.h"
+
---
+++ DARP_IPS/src/solvers/solver.h
@@ -0,0 +1,20 @@
+//
+// Created by Elahe Amiri on 2022-10-26.
+//
+
+#ifndef _SOLVER_H
+#define _SOLVER_H
+
+#include "solvers/ISUDAlgorithm.h"
+#include "solvers/CPLEXSubProblem.h"
+#include "solvers/LabelingSubProblem.h"
+#include "data/Instance.h"
+#include "solvers/MIPSolver.h"
+#include "solvers/GreedyModeler.h"
+
+class solver {
+public:
+};
+
+
+#endif //_SOLVER_H
