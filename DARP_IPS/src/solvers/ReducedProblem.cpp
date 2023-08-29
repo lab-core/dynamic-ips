7d977f59db3ecfdac1061fb03b885581782626d9
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2023-08-29 13:08:49 -0400

    Update ReducedProblem.cpp

--- DARP_IPS/src/solvers/ReducedProblem.cpp
+++ DARP_IPS/src/solvers/ReducedProblem.cpp
@@ -72,7 +72,7 @@ void ReducedProblem:: addZVar(PRequest &request) {
 void ReducedProblem::updateModel(PInstance &pInst, std::vector<PRoute> &routeSolution) {
     if (routesToAdd_.empty()) {
         std::cout << "There is no route to be added" << std::endl;
-        throw myTools::myException("The input route is empty, No new column is passed to be added", __LINE__);
+ //       throw myTools::myException("The input route is empty, No new column is passed to be added", __LINE__);
     }
 
     // add the new compatible column to the model
