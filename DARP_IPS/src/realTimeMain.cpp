dc497efa2880da5b92bede764bdcb13a3aa722b7
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2023-09-15 07:38:38 -0400

    setting

--- DARP_IPS/src/realTimeMain.cpp
+++ DARP_IPS/src/realTimeMain.cpp
@@ -12,7 +12,7 @@
 
 using namespace std::chrono;
 float saveTime = 3600;
-bool middleSave = true;
+bool middleSave = false;
 bool savePartial = false;
 std::string instNum = "10";
 int numVehicles = 2000;
