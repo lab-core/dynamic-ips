7d8525539e7bd68804061057dffd9bf9e1a58380
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2024-05-01 13:52:14 -0400

    setting

--- DARP_IPS/src/realTimeMain.cpp
+++ DARP_IPS/src/realTimeMain.cpp
@@ -68,7 +68,7 @@ int main(int argc, char** argv) {
     }*/
 
     for (auto & instanceName : instNames){
-        for (int i = 0; i < 4; ++i) {
+        for (int i = 0; i < 1; ++i) {
             for (int j = 0; j < 1; ++j){
                 std::this_thread::sleep_for(std::chrono::seconds(2));
                 // create output files for epoch results
@@ -80,7 +80,6 @@ int main(int argc, char** argv) {
                 PInstance mainInst = ReadWrite::readInstance(inputPaths.getInputInstanceData());
                 mainInst->nbVehicles_ = numVehicles;
                 ReadWrite::readParameters(inputPaths.getInputParamFile(), mainInst);
-                mainInst->parameters_->nbPick_ = i+1;
                 ReadWrite::readZones(inputPaths.getInputZones(), mainInst);
                 mainInst->parameters_->savePartial_ = savePartial;
                 mainInst->parameters_->mainAlgorithm_ = static_cast<MainAlgorithm>(mainAlgo);
