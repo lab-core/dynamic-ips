7b705fb638ba195be21ad0739d20625e3fac0a09
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2024-05-02 22:01:55 -0400

    Update run-vector.sh

--- DARP_IPS/run-vector.sh
+++ DARP_IPS/run-vector.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=32G
-#SBATCH --time=16:30:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=8G
+#SBATCH --time=0:30:00
 #SBATCH --array=1-24
 #SBATCH --output=/dev/null
 
