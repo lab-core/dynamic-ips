92f8d5e2a9785b3d1129d0e64af2887dbcaad9b1
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2024-05-07 21:52:42 -0400

    setting

--- DARP_IPS/src/solvers/GreedyModeler.cpp
+++ DARP_IPS/src/solvers/GreedyModeler.cpp
@@ -22,11 +22,11 @@ GreedyModeler::~GreedyModeler() {
 }
 
 void GreedyModeler::initialization(PInstance &PInst) {
-    PInst->selectedVehicles_.clear();
+//    PInst->selectedVehicles_.clear();
     for (auto & vehicleObj : PInst->vehicles_) {
         greedyRouteList_.emplace_back(std::make_shared<GreedyRoute>(vehicleObj, PInst, greedyLabelPool_, PInst->parameters_->greedyReOptimize_));
     }
-    PInst->selectedVehicles_.resize(PInst->nbVehicles_,0);
+//    PInst->selectedVehicles_.resize(PInst->nbVehicles_,0);
     if (positionList_.empty()){
         for (int i = 0; i < PInst->nbVehicles_; ++i)
             positionList_.emplace_back(std::make_shared<insertPosition>());
