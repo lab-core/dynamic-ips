89561fa0662e37855ea9489a04c1a7c7bb0190ba
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2024-05-21 00:31:41 +0200

    Update realTimeMain.cpp

--- DARP_IPS/src/realTimeMain.cpp
+++ DARP_IPS/src/realTimeMain.cpp
@@ -68,7 +68,7 @@ int main(int argc, char** argv) {
     }*/
 
     for (auto & instanceName : instNames){
-        for (int i = 0; i < 7; ++i) {
+        for (int i = 0; i < 8; ++i) {
             for (int j = 0; j < 2; ++j){
                 std::this_thread::sleep_for(std::chrono::seconds(2));
                 // create output files for epoch results
@@ -82,7 +82,10 @@ int main(int argc, char** argv) {
                 ReadWrite::readParameters(inputPaths.getInputParamFile(), mainInst);
 //                mainInst->parameters_->vehicleReturn_= j;
 //                mainInst->parameters_->nbPick_= j+1;
-                mainInst->parameters_->nbColumn_ = (i+1)*5;
+                if (i == 0)
+                    mainInst->parameters_->isTruncated_ = false;
+                else
+                    mainInst->parameters_->MaxLabel_ = i *5;
                 mainInst->parameters_->sortPath_ = static_cast<SortPaths>(j);
                 ReadWrite::readZones(inputPaths.getInputZones(), mainInst);
                 mainInst->parameters_->savePartial_ = savePartial;
