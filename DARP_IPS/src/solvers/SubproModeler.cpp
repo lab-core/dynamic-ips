60ad8699e33fe7697681292aa68da69284f50ba9
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2024-07-15 19:43:53 -0400

    debug

--- DARP_IPS/src/solvers/SubproModeler.cpp
+++ DARP_IPS/src/solvers/SubproModeler.cpp
@@ -5,7 +5,6 @@
 #include "SubproModeler.h"
 
 SubproModeler::SubproModeler(PVehicle &vehicle) : Vehicle_(&(*vehicle)) {
-    std::cout << "residam" << std::endl;
     subGraph_ = std::make_shared<Graph>();
     nbNegativeColumns_ = 0;
     nbTotalRequest_ = 0;
