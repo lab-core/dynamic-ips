c95ea01f38bd0c65b7e9d3343dae4afa413c6555
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2024-07-20 12:15:44 -0400

    test

--- DARP_IPS/src/realTimeMain.cpp
+++ DARP_IPS/src/realTimeMain.cpp
@@ -69,7 +69,7 @@ int main(int argc, char** argv) {
     }*/
 
     for (auto & instanceName : instNames){
-        for (int i = 0; i < 2; ++i) {
+        for (int i = 0; i < 1; ++i) {
             for (int j = 0; j < 1; ++j){
                 std::this_thread::sleep_for(std::chrono::seconds(2));
                 // create output files for epoch results
@@ -90,10 +90,10 @@ int main(int argc, char** argv) {
                     mainInst->parameters_->MaxLabel_ = i *5;*/
  //               mainInst->parameters_->MaxLabel_ = (i + 1) *5;
  //               mainInst->parameters_->sortPath_ = static_cast<SortPaths>(j);
-                if (i == 0)
+                /*if (i == 0)
                     mainInst->parameters_->isDropPickPossible_ = true;
                 else
-                    mainInst->parameters_->isDropPickPossible_ = false;
+                    mainInst->parameters_->isDropPickPossible_ = false;*/
                 ReadWrite::readZones(inputPaths.getInputZones(), mainInst);
                 mainInst->parameters_->savePartial_ = savePartial;
                 mainInst->parameters_->mainAlgorithm_ = static_cast<MainAlgorithm>(mainAlgo);
