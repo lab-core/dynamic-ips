da9f96a8a674f46452ee0bf7c83a13a624cb342d
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2024-07-23 14:54:14 -0400

    Update run-vector.sh

--- DARP_IPS/run-vector.sh
+++ DARP_IPS/run-vector.sh
@@ -2,7 +2,7 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=64G
 #SBATCH --time=2:20:00
-#SBATCH --array=1-3
+#SBATCH --array=1-24
 #SBATCH --output=/dev/null
 
 
@@ -10,14 +10,14 @@ module load eigen
 module load gcc
 
 i=1
-for vehicles in sufficient_manhattan-vehicles-300
-#for vehicles in manhattan-vehicles
+#for vehicles in sufficient_manhattan-vehicles-300
+for vehicles in manhattan-vehicles
 do
   for mode in 1
   do
     for algorithm in 2
     do
-      for directory in Instances_12-14
+      for directory in Instances-120
       do
 #        for instance in 20150828_12-120m 20151130_12-120m 20160222_12-120m
 #        for instance in 20160316_12-120m 20151110_12-120m 20160512_12-120m
@@ -28,7 +28,7 @@ do
 #        for instance in 20150926_07-120m 20151025_07-120m 20151230_07-120m 20160109_07-120m 20160521_07-120m
 
         do
-          for num_vehicles in 1000
+          for num_vehicles in 2000
           do
             if [ $SLURM_ARRAY_TASK_ID -eq $i ]
             then
