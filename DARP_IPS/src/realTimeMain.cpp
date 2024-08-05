f3a9d0b1ec1e3e9060a52edbe92a4998ff45bd5d
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2024-08-05 07:07:42 -0400

    setting

--- DARP_IPS/src/realTimeMain.cpp
+++ DARP_IPS/src/realTimeMain.cpp
@@ -69,8 +69,8 @@ int main(int argc, char** argv) {
     }*/
 
     for (auto & instanceName : instNames){
-        for (int i = 0; i < 7; ++i) {
-            for (int j = 0; j < 3; ++j){
+        for (int i = 0; i < 1; ++i) {
+            for (int j = 0; j < 1; ++j){
                 std::this_thread::sleep_for(std::chrono::seconds(2));
                 // create output files for epoch results
                 inputPaths.initializeInputs(instFolder, instanceName);
@@ -92,8 +92,8 @@ int main(int argc, char** argv) {
                     mainInst->parameters_->isTruncated_ = false;
                 else
                     mainInst->parameters_->MaxLabel_ = i *5;*/
-                mainInst->parameters_->MaxLabel_ = (i + 1) *5;
-                mainInst->parameters_->sortPath_ = static_cast<SortPaths>(j);
+//                mainInst->parameters_->MaxLabel_ = (i + 1) *5;
+//                mainInst->parameters_->sortPath_ = static_cast<SortPaths>(j);
                 /*if (i == 0)
                     mainInst->parameters_->oneIter_ = true;
                 else
