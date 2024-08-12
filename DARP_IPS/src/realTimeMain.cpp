01dcf57b74015581ba8f3d9e3c073f687239b10e
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2024-08-12 00:32:21 -0400

    setting

--- DARP_IPS/src/realTimeMain.cpp
+++ DARP_IPS/src/realTimeMain.cpp
@@ -69,8 +69,8 @@ int main(int argc, char** argv) {
     }*/
 
     for (auto & instanceName : instNames){
-        for (int i = 0; i < 1; ++i) {
-            for (int j = 0; j < 1; ++j){
+        for (int i = 0; i < 7; ++i) {
+            for (int j = 0; j < 3; ++j){
                 std::this_thread::sleep_for(std::chrono::seconds(2));
                 // create output files for epoch results
                 inputPaths.initializeInputs(instFolder, instanceName);
@@ -82,6 +82,10 @@ int main(int argc, char** argv) {
                 if (!solveEpoch)
                     mainInst->nbVehicles_ = numVehicles;
                 ReadWrite::readParameters(inputPaths.getInputParamFile(), mainInst);
+
+                mainInst->parameters_->MaxLabel_ = (i + 1) *5;
+                mainInst->parameters_->sortPath_ = static_cast<SortPaths>(j);
+
                 /*if (i == 1) {
                     mainInst->parameters_->isSuccessorsLimited_ = true;
                 }
@@ -94,8 +98,7 @@ int main(int argc, char** argv) {
                     mainInst->parameters_->isTruncated_ = false;
                 else
                     mainInst->parameters_->MaxLabel_ = i *5;*/
-//                mainInst->parameters_->MaxLabel_ = (i + 1) *5;
-//                mainInst->parameters_->sortPath_ = static_cast<SortPaths>(j);
+
                 /*if (i == 0)
                     mainInst->parameters_->oneIter_ = true;
                 else
