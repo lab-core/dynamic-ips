0c083327e4a417ada0626e407892d0d29c8a713f
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2024-08-16 13:53:28 -0400

    setting

--- DARP_IPS/src/realTimeMain.cpp
+++ DARP_IPS/src/realTimeMain.cpp
@@ -69,8 +69,8 @@ int main(int argc, char** argv) {
     }*/
 
     for (auto & instanceName : instNames){
-        for (int i = 0; i < 2; ++i) {
-            for (int j = 0; j < 2; ++j){
+        for (int i = 0; i < 7; ++i) {
+            for (int j = 0; j < 3; ++j){
                 std::this_thread::sleep_for(std::chrono::seconds(2));
                 // create output files for epoch results
                 inputPaths.initializeInputs(instFolder, instanceName);
@@ -83,10 +83,10 @@ int main(int argc, char** argv) {
                     mainInst->nbVehicles_ = numVehicles;
                 ReadWrite::readParameters(inputPaths.getInputParamFile(), mainInst);
 
-                /*mainInst->parameters_->MaxLabel_ = (i + 1) *5;
-                mainInst->parameters_->sortPath_ = static_cast<SortPaths>(j);*/
+                mainInst->parameters_->MaxLabel_ = (i + 1) *5;
+                mainInst->parameters_->sortPath_ = static_cast<SortPaths>(j);
 
-                if (i == 1) {
+                /*if (i == 1) {
                     mainInst->parameters_->dynamicPricing_ = true;
                 }
                 else {
@@ -98,7 +98,7 @@ int main(int argc, char** argv) {
                 }
                 else {
                     mainInst->parameters_->isDropPickPossible_ = false;
-                }
+                }*/
 //                mainInst->parameters_->vehicleReturn_= j;
  //               mainInst->parameters_->nbPick_= i+1;
                 /*if (i == 0)
