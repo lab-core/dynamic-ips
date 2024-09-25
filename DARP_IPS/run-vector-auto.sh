63d8680d1014883e7d53e6f9bdf8bd4649e5f389
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2024-09-24 23:44:19 -0400

    setting

--- DARP_IPS/run-vector-auto.sh
+++ DARP_IPS/run-vector-auto.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --time=5:30:00
+#SBATCH --time=2:30:00
 #SBATCH --account=def-legraina
 #SBATCH --mem=32G
 #SBATCH --cpus-per-task=16
@@ -11,8 +11,8 @@ module load eigen
 module load gcc
 cmake --build cmake-build-release --target all
 
-DIRECTORY="Instances_5h"
-MAIN_DIR="datasets/Instances_5h"
+DIRECTORY="Instances-120"
+MAIN_DIR="datasets/Instances-120"
 
 # Dynamically create the INSTANCES array with paths to each test subdirectory
 INSTANCES=($(find ./$MAIN_DIR -mindepth 1 -maxdepth 1 -type d -print | sort))
@@ -20,14 +20,14 @@ INSTANCES=($(find ./$MAIN_DIR -mindepth 1 -maxdepth 1 -type d -print | sort))
 i=1
 for vehicles in manhattan-vehicles
 do
-  for mode in 2 1
+  for mode in 1
   do
     for algorithm in 2
     do
       for instance_path in "${INSTANCES[@]}";
       do
         instance=$(basename "$instance_path")
-        for num_vehicles in 1500
+        for num_vehicles in 2000
 #        for num_vehicles in 1700 1800 1900 2000 2100 2200 2300 2400 2500 2600 2700 2800 2900 3000 3100 3200 3300 3400 3500
         do
           if [ $SLURM_ARRAY_TASK_ID -eq $i ]
