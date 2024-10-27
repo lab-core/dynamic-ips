bee6dc50e3ef42119314a17a57707f49e4d6c719
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2024-10-27 16:47:36 -0400

    test

--- DARP_IPS/src/realTimeMain.cpp
+++ DARP_IPS/src/realTimeMain.cpp
@@ -69,7 +69,7 @@ int main(int argc, char** argv) {
     }*/
 
     for (auto & instanceName : instNames){
-        for (int i = 0; i < 1; ++i) {
+        for (int i = 0; i < 3; ++i) {
             for (int j = 0; j < 1; ++j){
                 std::this_thread::sleep_for(std::chrono::seconds(2));
                 // create output files for epoch results
