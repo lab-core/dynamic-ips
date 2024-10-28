5097aad9b15467772ad6dcb75b4fe2ce727f7a50
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2024-10-28 12:09:57 -0400

    2 hour setting

--- DARP_IPS/src/realTimeMain.cpp
+++ DARP_IPS/src/realTimeMain.cpp
@@ -17,7 +17,7 @@ bool savePartial = false ;
 std::string instNum = "1";
 int numEpochTests = 30;
 int numVehicles;
-bool solveEpoch = true;
+bool solveEpoch = false;
 
 int main(int argc, char** argv) {
     std::ios_base::sync_with_stdio(false);
@@ -69,7 +69,7 @@ int main(int argc, char** argv) {
     }*/
 
     for (auto & instanceName : instNames){
-        for (int i = 0; i < 5; ++i) {
+        for (int i = 0; i < 1; ++i) {
             for (int j = 0; j < 1; ++j){
                 std::this_thread::sleep_for(std::chrono::seconds(2));
                 // create output files for epoch results
@@ -98,12 +98,12 @@ int main(int argc, char** argv) {
                     mainInst->parameters_->pruneArcs_ = true;
                     mainInst->parameters_->discardSuboptimalPath_ = true;
                 }*/
-                if (i < 4)
+                /*if (i < 4)
                     mainInst->parameters_->nbPick_= i+1;
                 if (i == 4) {
                     mainInst->parameters_->nbPick_= 4;
                     mainInst->parameters_->dynamicPricing_ = true;
-                }
+                }*/
 
                 ReadWrite::readZones(inputPaths.getInputZones(), mainInst);
                 mainInst->parameters_->savePartial_ = savePartial;
