7e7d73385903b4074103c639299a57fc7790cfa4
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2024-11-11 22:41:30 -0500

    test

--- DARP_IPS/run-all2.sh
+++ DARP_IPS/run-all2.sh
@@ -12,15 +12,3 @@ bin/realtime_DARP sufficient_manhattan-vehicles-300 Instances-120 20160512_07-12
 bin/realtime_DARP sufficient_manhattan-vehicles-300 Instances-120 20160521_07-120m 1400 2 1
 bin/realtime_DARP sufficient_manhattan-vehicles-300 Instances-120 20160613_07-120m 1400 2 1
 bin/realtime_DARP sufficient_manhattan-vehicles-300 Instances-120 20160628_07-120m 1400 2 1
-bin/realtime_DARP sufficient_manhattan-vehicles-300 Instances-120 20160109_07-120m 1400 3 1
-bin/realtime_DARP sufficient_manhattan-vehicles-300 Instances-120 20160129_07-120m 1400 3 1
-bin/realtime_DARP sufficient_manhattan-vehicles-300 Instances-120 20160222_07-120m 1400 3 1
-bin/realtime_DARP sufficient_manhattan-vehicles-300 Instances-120 20160225_07-120m 1400 3 1
-bin/realtime_DARP sufficient_manhattan-vehicles-300 Instances-120 20160316_07-120m 1400 3 1
-bin/realtime_DARP sufficient_manhattan-vehicles-300 Instances-120 20160329_07-120m 1400 3 1
-bin/realtime_DARP sufficient_manhattan-vehicles-300 Instances-120 20160401_07-120m 1400 3 1
-bin/realtime_DARP sufficient_manhattan-vehicles-300 Instances-120 20160427_07-120m 1400 3 1
-bin/realtime_DARP sufficient_manhattan-vehicles-300 Instances-120 20160512_07-120m 1400 3 1
-bin/realtime_DARP sufficient_manhattan-vehicles-300 Instances-120 20160521_07-120m 1400 3 1
-bin/realtime_DARP sufficient_manhattan-vehicles-300 Instances-120 20160613_07-120m 1400 3 1
-bin/realtime_DARP sufficient_manhattan-vehicles-300 Instances-120 20160628_07-120m 1400 3 1
