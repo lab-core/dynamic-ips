06e01ea9ab7aca6c38032cd782ab03afd67123d3
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2024-11-17 15:25:17 -0500

    Update run-vector-auto_1.sh

--- DARP_IPS/run-vector-auto_1.sh
+++ DARP_IPS/run-vector-auto_1.sh
@@ -2,7 +2,7 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=32G
 #SBATCH --time=2:20:00
-#SBATCH --array=1-1
+#SBATCH --array=1-9
 #SBATCH --output=/dev/null
 
 module load eigen
@@ -19,9 +19,33 @@ do
       for directory in Instances_12-14
       do
         # First group of instances with num_vehicles=1000
-        for instance in 20151230_12-120m
+        for instance in 20150828_12-120m 20151130_12-120m 20160222_12-120m 20151230_12-120m
         do
           num_vehicles=1000
+          if [ $SLURM_ARRAY_TASK_ID -eq $i ]
+          then
+            bin/realtime_DARP $vehicles $directory $instance $num_vehicles $algorithm $mode
+#           bin/realtime_DARP $directory $instance $num_vehicles $algorithm $mode > "/scratch/amirelah/dynamic-ips/${directory}_${instance}_${num_vehicles}_${algorithm}_${mode}.txt" 2>&1
+          fi
+          (( i = $i + 1 ))
+        done
+
+        # Second group of instances with num_vehicles=1100
+        for instance in 20160316_12-120m 20160512_12-120m
+        do
+          num_vehicles=1100
+          if [ $SLURM_ARRAY_TASK_ID -eq $i ]
+          then
+            bin/realtime_DARP $vehicles $directory $instance $num_vehicles $algorithm $mode
+#           bin/realtime_DARP $directory $instance $num_vehicles $algorithm $mode > "/scratch/amirelah/dynamic-ips/${directory}_${instance}_${num_vehicles}_${algorithm}_${mode}.txt" 2>&1
+          fi
+          (( i = $i + 1 ))
+        done
+
+        # Third group of instances with num_vehicles=1400
+        for instance in 20160521_12-120m 20151025_12-120m 20150926_12-120m
+        do
+          num_vehicles=1400
           if [ $SLURM_ARRAY_TASK_ID -eq $i ]
           then
             bin/realtime_DARP $vehicles $directory $instance $num_vehicles $algorithm $mode
