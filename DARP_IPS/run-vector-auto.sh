e420a5cd306720bf1160770f26d63be3ad5a6e8b
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2025-01-14 14:50:15 -0500

    setting

--- DARP_IPS/run-vector-auto.sh
+++ DARP_IPS/run-vector-auto.sh
@@ -18,7 +18,7 @@ MAIN_DIR="datasets/Instances-120"
 INSTANCES=($(find ./$MAIN_DIR -mindepth 1 -maxdepth 1 -type d -print | sort))
 
 i=1
-for vehicles in manhattan-vehicles
+for vehicles in sufficient_manhattan-vehicles-300
 do
   for mode in 2
   do
@@ -27,7 +27,7 @@ do
       for instance_path in "${INSTANCES[@]}";
       do
         instance=$(basename "$instance_path")
-        for num_vehicles in 2000
+        for num_vehicles in 1500
 #        for num_vehicles in 1700 1800 1900 2000 2100 2200 2300 2400 2500 2600 2700 2800 2900 3000 3100 3200 3300 3400 3500
         do
           if [ $SLURM_ARRAY_TASK_ID -eq $i ]
