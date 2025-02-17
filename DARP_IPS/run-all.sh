c85e130dec8170715c47a70cc2b0c4b33c292198
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2025-02-17 10:09:43 -0500

    Update run-all.sh

--- DARP_IPS/run-all.sh
+++ DARP_IPS/run-all.sh
@@ -10,10 +10,30 @@ param_dir="Parameters.txt"
 DIRECTORY="Instances_12-14_Last"
 MAIN_DIR="datasets/$DIRECTORY"
 
-# Dynamically create the INSTANCES array with paths to each test subdirectory
-INSTANCES=($(find "./$MAIN_DIR" -mindepth 1 -maxdepth 1 -type d -print | sort))
+# List of instances based on the previous grouping approach
+instances_1000=("20150828_12-120m" "20151130_12-120m" "20160222_12-120m" "20151230_12-120m")
+instances_1100=("20160316_12-120m" "20160512_12-120m")
+instances_1400=("20160521_12-120m" "20151025_12-120m" "20150926_12-120m")
 
-for instance in "${INSTANCES[@]}"; do
-  instance_basename=$(basename "$instance")
-  "$program" "$map_file" "$DIRECTORY" "$instance_basename" $args "$param_dir" 0
+# Define mode and corresponding algorithm
+mode=2
+algorithm=6
+
+# Define parameter files for mode 2
+param_files=("Param_mode_2a.txt" "Param_mode_2b.txt" "Param_mode_2c.txt" "Param_mode_2d.txt")
+
+# Run the program for each instance and parameter file
+for param_file in "${param_files[@]}"; do
+  for instance in "${instances_1000[@]}"; do
+    "$program" "$map_file" "$DIRECTORY" "$instance" 1000 "$algorithm" "$mode" "$param_file" 0
+  done
+  for instance in "${instances_1100[@]}"; do
+    "$program" "$map_file" "$DIRECTORY" "$instance" 1100 "$algorithm" "$mode" "$param_file" 0
+  done
+  for instance in "${instances_1400[@]}"; do
+    "$program" "$map_file" "$DIRECTORY" "$instance" 1400 "$algorithm" "$mode" "$param_file" 0
+  done
 done
+
+# Add a delay to avoid overloading the system
+sleep 60
