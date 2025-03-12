3db6659cb1636c46079b5c07a38a30b7c2dad991
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2025-03-12 02:24:21 -0400

    setting

--- DARP_IPS/run-small-canada.sh
+++ DARP_IPS/run-small-canada.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --account=ctb-dionneg1
-#SBATCH --mem=24G
-#SBATCH --time=0:30:00
-#SBATCH --array=1-540
+#SBATCH --mem=16G
+#SBATCH --time=0:40:00
+#SBATCH --array=1-810
 #SBATCH --output=/dev/null
 
 # Load required modules
@@ -20,7 +20,7 @@ algorithms[1]=2  # Mode 1 -> Algorithm 2
 
 # Define parameter files for each mode
 declare -A param_files
-param_files[1]="initial dominance"  # Mode 1 has two parameter files
+param_files[1]="prune_node prune_arc prune_path"  # Mode 1 has two parameter files
 
 # Dynamically create the INSTANCES array with paths to each test subdirectory
 INSTANCES=($(find "./$main_dir" -mindepth 1 -maxdepth 1 -type d -print | sort))
