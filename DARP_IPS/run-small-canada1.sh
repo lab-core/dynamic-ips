d15a09b7e71ea70360e128dd9af145780aa6f9f8
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2025-03-27 09:51:04 -0400

    Update run-small-canada1.sh

--- DARP_IPS/run-small-canada1.sh
+++ DARP_IPS/run-small-canada1.sh
@@ -3,7 +3,7 @@
 #SBATCH --mem=32G
 #SBATCH --cpus-per-task=16
 #SBATCH --time=1:10:00
-#SBATCH --array=1-4
+#SBATCH --array=1-5
 #SBATCH --output=/dev/null
 
 # Load required modules
@@ -21,7 +21,7 @@ algorithms[1]=2  # Mode 1 -> Algorithm 2
 
 # Define parameter files for each mode
 declare -A param_files
-param_files[1]="initial"  # Mode 1 has two parameter files
+param_files[1]="nbPick1 nbPick2 nbPick3 nbPick4 truncate"  # Mode 1 has two parameter files
 
 # Dynamically create the INSTANCES array with paths to each test subdirectory
 #INSTANCES=($(find "./$main_dir" -mindepth 1 -maxdepth 1 -type d -print | sort))
