e89f23335de506c7d22c7826670d9602efd6c9a7
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2025-04-03 20:07:20 -0400

    pick setting

--- DARP_IPS/run-small-canada.sh
+++ DARP_IPS/run-small-canada.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --mem=16G
+#SBATCH --mem=64G
 #SBATCH --cpus-per-task=16
-#SBATCH --time=0:05:00
-#SBATCH --array=1-270
+#SBATCH --time=4:35:00
+#SBATCH --array=1-18
 #SBATCH --output=/dev/null
 
 # Load required modules
@@ -20,11 +20,11 @@ algorithms[1]=2  # Mode 1 -> Algorithm 2
 
 # Define parameter files for each mode
 declare -A param_files
-param_files[1]="dynamic"  # Mode 1 has two parameter files
+param_files[1]="nbPick3 nbPick4"  # Mode 1 has two parameter files
 
 # Dynamically create the INSTANCES array with paths to each test subdirectory
 INSTANCES=($(find "./$main_dir" -mindepth 1 -maxdepth 1 -type d -print | sort))
-#instances=("20160316_12-120m_114" "20160316_12-120m_120" "20160512_12-120m_105" "20160512_12-120m_107" "20160512_12-120m_108" "20160512_12-120m_109" "20160512_12-120m_110" "20160512_12-120m_116" "20160521_12-120m_98")
+instances=("20151025_12-120m_87" "20151230_12-120m_92" "20151130_12-120m_70" "20160521_12-120m_85" "20160222_12-120m_59" "20150926_12-120m_100" "20160316_12-120m_51" "20150828_12-120m_59" "20160512_12-120m_73")
 
 # Define vehicle counts
 num_vehicles_list=(1500)
@@ -36,9 +36,9 @@ i=1
 for mode in 1; do
   algorithm=${algorithms[$mode]}  # Select algorithm for the current mode
   for param_dir in ${param_files[$mode]}; do
-    for instance_path in "${INSTANCES[@]}"; do
-      instance=$(basename "$instance_path")
-#    for instance in "${instances[@]}"; do
+#    for instance_path in "${INSTANCES[@]}"; do
+#      instance=$(basename "$instance_path")
+    for instance in "${instances[@]}"; do
       jobs[$i]="$vehicles_2 $directory $instance 1500 $algorithm $mode $param_dir 1"
       ((i++))
     done
