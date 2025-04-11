6de7b3ffbc424e3d73ccd04aef38ef8a276e5c71
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2025-04-10 22:07:41 -0400

    Update run-vector-auto.sh

--- DARP_IPS/run-vector-auto.sh
+++ DARP_IPS/run-vector-auto.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --mem=32G
+#SBATCH --mem=18G
 #SBATCH --cpus-per-task=16
 #SBATCH --time=2:15:00
-#SBATCH --array=1-9
+#SBATCH --array=1-12
 #SBATCH --output=/dev/null
 
 # Load required modules
@@ -11,7 +11,7 @@ module load eigen gcc
 # Define fixed parameters
 vehicles_1="manhattan-vehicles"
 vehicles_2="sufficient_manhattan-vehicles-300"
-directory="Instances_12-14"
+directory="Instances-120"
 main_dir="datasets/$directory"
 
 # Define algorithms for each mode
@@ -21,14 +21,14 @@ algorithms[2]=6  # Mode 2 -> Algorithm 6
 
 # Define parameter files for each mode
 declare -A param_files
-param_files[1]="ACG-LP ACG-AUX"  # Mode 1 has two parameter files
-param_files[2]="ACG-AUXP"  # Mode 2 has three parameter files
+param_files[1]="Ab_drop Ab_truncate"  # Mode 1 has two parameter files
+param_files[2]="ACG-LP ACG-CP ACG-AUXP"  # Mode 2 has three parameter files
 param_files[3]="Param_mode_1r"
 param_files[4]="Param_mode_2r"
 
 # Dynamically create the INSTANCES array with paths to each test subdirectory
 INSTANCES=($(find "./$main_dir" -mindepth 1 -maxdepth 1 -type d -print | sort))
-#instances=("20160316_12-120m" "20160512_12-120m")
+instances=("20150706_07-120m 20150828_07-120m 20151025_07-120m 20160109_07-120m 20160316_07-120m 20160401_07-120m")
 
 # Define vehicle counts
 num_vehicles_list=(1100)
@@ -39,11 +39,11 @@ num_vehicles_list=(1100)
 declare -a jobs
 i=1
 
-for mode in 2; do
+for mode in 1; do
   algorithm=${algorithms[$mode]}  # Select algorithm for the current mode
-  for instance_path in "${INSTANCES[@]}"; do
-    instance=$(basename "$instance_path")
-#  for instance in "${instances[@]}"; do
+#  for instance_path in "${INSTANCES[@]}"; do
+#    instance=$(basename "$instance_path")
+  for instance in "${instances[@]}"; do
     for param_dir in ${param_files[$mode]}; do
 
 #    param_dir=${param_files[$mode]}
@@ -51,7 +51,7 @@ for mode in 2; do
 #    ((i++))
 
 #    param_dir=${param_files[$((mode+2))]}
-    jobs[$i]="$vehicles_2 $directory $instance 1100 $algorithm $mode $param_dir 1"
+    jobs[$i]="$vehicles_1 $directory $instance 2000 $algorithm $mode $param_dir 1"
     ((i++))
     done
   done
