389b7019c8770a51561b9036597987ea9857ead2
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2025-04-27 09:38:01 -0400

    Update run-vector-auto.sh

--- DARP_IPS/run-vector-auto.sh
+++ DARP_IPS/run-vector-auto.sh
@@ -2,7 +2,7 @@
 #SBATCH --mem=24G
 #SBATCH --cpus-per-task=16
 #SBATCH --time=2:15:00
-#SBATCH --array=1-48
+#SBATCH --array=1-24
 #SBATCH --output=/dev/null
 
 # Load required modules
@@ -21,7 +21,7 @@ algorithms[2]="6 3"  # Mode 2 -> Algorithm 6
 
 # Define parameter files for each mode
 declare -A param_files
-param_files[1]="Ab_dynamic"  # Mode 1 has two parameter files
+param_files[1]="return2"  # Mode 1 has two parameter files
 param_files[2]="ACG-LP"  # Mode 2 has three parameter files
 
 # Dynamically create the INSTANCES array with paths to each test subdirectory
@@ -36,7 +36,7 @@ vehicle_counts=(2000)
 declare -a jobs
 i=1
 
-for mode in 2; do
+for mode in 1; do
   for algorithm in ${algorithms[$mode]}; do  # Select algorithm for the current mode
     for vehicle_count in "${vehicle_counts[@]}"; do
       for instance_path in "${INSTANCES[@]}"; do
