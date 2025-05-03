b135fa94163ebb8579852f334f77b8a76302a996
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2025-05-03 14:59:38 -0400

    sort test

--- DARP_IPS/run-vector-auto_1.sh
+++ DARP_IPS/run-vector-auto_1.sh
@@ -2,7 +2,7 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=18G
 #SBATCH --time=2:15:00
-#SBATCH --array=1-27
+#SBATCH --array=1-18
 #SBATCH --output=/dev/null
 
 # Load required modules
@@ -20,7 +20,7 @@ algorithms[2]="6"  # Mode 2 -> Algorithm 6
 # Define parameter files for each mode
 declare -A param_files
 param_files[1]="return1 return3 return4"  # Mode 1 has two parameter files
-param_files[2]="ACG-CP ACG-AUXP"  # Mode 2 has three parameter files
+param_files[2]="ACG-LP-S ACG-LP-L"  # Mode 2 has three parameter files
 
 # Define instance groups and corresponding vehicle counts
 instances_1000=("20150828_12-120m" "20151130_12-120m" "20160222_12-120m" "20151230_12-120m")
@@ -31,7 +31,7 @@ instances_1400=("20160521_12-120m" "20151025_12-120m" "20150926_12-120m")
 declare -a jobs
 i=1
 
-for mode in 1; do
+for mode in 2; do
   for algorithm in ${algorithms[$mode]}; do  # Select algorithm for the current mode
     for param_dir in ${param_files[$mode]}; do  # Iterate over multiple parameter files for each mode
 
