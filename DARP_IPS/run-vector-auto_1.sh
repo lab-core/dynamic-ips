dca00ca19298ef2c46691a3286c0fa83d9321745
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2025-05-04 13:39:55 -0400

    Update run-vector-auto_1.sh

--- DARP_IPS/run-vector-auto_1.sh
+++ DARP_IPS/run-vector-auto_1.sh
@@ -2,7 +2,7 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=18G
 #SBATCH --time=2:15:00
-#SBATCH --array=1-18
+#SBATCH --array=1-9
 #SBATCH --output=/dev/null
 
 # Load required modules
@@ -15,12 +15,12 @@ directory="Instances_12-14"
 # Define algorithms for each mode
 declare -A algorithms
 algorithms[1]="2"  # Mode 1 -> Algorithm 2
-algorithms[2]="6"  # Mode 2 -> Algorithm 6
+algorithms[2]="3"  # Mode 2 -> Algorithm 6
 
 # Define parameter files for each mode
 declare -A param_files
 param_files[1]="return1 return3 return4"  # Mode 1 has two parameter files
-param_files[2]="ACG-LP-S ACG-LP-L"  # Mode 2 has three parameter files
+param_files[2]="ACG-LP"  # Mode 2 has three parameter files
 
 # Define instance groups and corresponding vehicle counts
 instances_1000=("20150828_12-120m" "20151130_12-120m" "20160222_12-120m" "20151230_12-120m")
