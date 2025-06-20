b5bb6ec9f50e80c183439f93c22e5a95ce5885b4
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2025-06-20 00:35:59 -0400

    Update MasterAlgorithm.cpp

--- DARP_IPS/src/solvers/MasterAlgorithm.cpp
+++ DARP_IPS/src/solvers/MasterAlgorithm.cpp
@@ -1445,7 +1445,6 @@ void MasterAlgorithm::setAvailableTime(const PInstance &pInst, float elapsedTime
     }
     else
         availableTime_ = LARGE_CONSTANT;
-    availableTime_ = LARGE_CONSTANT;
 }
 
 
