1de77e2978502d192eff60fe0912acb6d795838d
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2025-09-04 00:34:30 -0400

    setting

--- DARP_IPS/run-groups.sh
+++ DARP_IPS/run-groups.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #SBATCH --cpus-per-task=16
-#SBATCH --mem=90G
+#SBATCH --mem=36G
 #SBATCH --time=4:10:00
 #SBATCH --array=1-54
 #SBATCH --error=slurm-%A_%a.err
@@ -19,9 +19,9 @@ exe="bin/realtime_DARP"
 G1_vehicle_folder="vehicles_byDemand_w11"
 G1_paramfile="AnyParameters"
 G1_vehicle_counts=(1500 1600 1700)
-G1_algorithms=(2)
-G1_modes=(1)
-G1_scenarios=("Rebalance_no")
+G1_algorithms=(6)
+G1_modes=(2)
+G1_scenarios=("Dynamic" "Partial")
 G1_inst_folder="Instances_4h-11"
 G1_instances=("20160329_11-240m" "20160628_11-240m")
 
@@ -29,9 +29,9 @@ G1_instances=("20160329_11-240m" "20160628_11-240m")
 G2_vehicle_folder="vehicles_byDemand_w11"
 G2_paramfile="AnyParameters"
 G2_vehicle_counts=(1600 1700 1800)
-G2_algorithms=(2)
-G2_modes=(1)
-G2_scenarios=("Rebalance_no")
+G2_algorithms=(6)
+G2_modes=(2)
+G2_scenarios=("Dynamic" "Partial")
 G2_inst_folder="Instances_4h-11"
 G2_instances=("20150917_11-240m")
 
