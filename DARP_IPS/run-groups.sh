c78ba911a5cfe4ebea99e939fcc7d86eb00269c3
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2025-09-04 01:22:10 -0400

    setting

--- DARP_IPS/run-groups.sh
+++ DARP_IPS/run-groups.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=36G
-#SBATCH --time=4:10:00
-#SBATCH --array=1-54
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=8G
+#SBATCH --time=0:30:00
+#SBATCH --array=1-9
 #SBATCH --error=slurm-%A_%a.err
 
 # Modules and binary
@@ -19,9 +19,9 @@ exe="bin/realtime_DARP"
 G1_vehicle_folder="vehicles_byDemand_w11"
 G1_paramfile="AnyParameters"
 G1_vehicle_counts=(1500 1600 1700)
-G1_algorithms=(6)
+G1_algorithms=(0)
 G1_modes=(2)
-G1_scenarios=("SP_10" "SP_20" "SP_30" "SP_40" "SP_50" "SP_150")
+G1_scenarios=("greedy")
 G1_inst_folder="Instances_4h-11"
 G1_instances=("20160329_11-240m" "20160628_11-240m")
 
@@ -29,9 +29,9 @@ G1_instances=("20160329_11-240m" "20160628_11-240m")
 G2_vehicle_folder="vehicles_byDemand_w11"
 G2_paramfile="AnyParameters"
 G2_vehicle_counts=(1600 1700 1800)
-G2_algorithms=(6)
+G2_algorithms=(0)
 G2_modes=(2)
-G2_scenarios=("SP_10" "SP_20" "SP_30" "SP_40" "SP_50" "SP_150")
+G2_scenarios=("greedy")
 G2_inst_folder="Instances_4h-11"
 G2_instances=("20150917_11-240m")
 
