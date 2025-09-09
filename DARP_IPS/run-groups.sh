99f3577f15fcfd3dd0e9056413f4dab6c420472b
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2025-09-09 13:59:18 -0400

    setting

--- DARP_IPS/run-groups.sh
+++ DARP_IPS/run-groups.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=24G
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=36G
 #SBATCH --time=2:10:00
 #SBATCH --array=1-200
 #SBATCH --error=slurm-%A_%a.err
@@ -21,7 +21,7 @@ G1_paramfile="AnyParameters"
 G1_vehicle_counts=(1500 1600 1700)
 G1_algorithms=(6)
 G1_modes=(2)
-G1_scenarios=("SP_10" "SP_20" "SP_150" "Partial")
+G1_scenarios=("SP_150" "Partial")
 #G1_scenarios=("Rebalance_1" "Rebalance_2" "Rebalance_3" "Rebalance_4" "Rebalance_5")
 #G1_scenarios=("Rebalance_no")
 G1_inst_folder="Instances_4h-11"
@@ -33,7 +33,7 @@ G2_paramfile="AnyParameters"
 G2_vehicle_counts=(1600 1700 1800)
 G2_algorithms=(6)
 G2_modes=(2)
-G2_scenarios=("SP_10" "SP_20" "SP_150" "Partial")
+G2_scenarios=("SP_150" "Partial")
 #G2_scenarios=("Rebalance_1" "Rebalance_2" "Rebalance_3" "Rebalance_4" "Rebalance_5")
 #G2_scenarios=("Rebalance_no")
 G2_inst_folder="Instances_4h-11"
@@ -103,7 +103,7 @@ add_group() {
       for s in "${scens_ref[@]}"; do
         for c in "${counts_ref[@]}"; do
           for inst in "${insts_ref[@]}"; do
-            jobs+=("$exe --vehicle-folder $vehicle_folder --inst-folder $inst_folder --instance-name $inst --num-vehicles $c --main-algo $a --sol-mode $m --paramfile $paramfile --scenario $s --save-scratch 1 --initial-state 0")
+            jobs+=("$exe --vehicle-folder $vehicle_folder --inst-folder $inst_folder --instance-name $inst --num-vehicles $c --main-algo $a --sol-mode $m --paramfile $paramfile --scenario $s --save-scratch 1 --initial-state 1")
           done
         done
       done
