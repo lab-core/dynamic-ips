6a0bd2906f1f2da45be4505d8a923c2f32fee1d5
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2025-09-10 01:06:22 -0400

    setting

--- DARP_IPS/src/solvers/CG_Algorithm.cpp
+++ DARP_IPS/src/solvers/CG_Algorithm.cpp
@@ -87,10 +87,12 @@ void CG_Algorithm::initializationGurobi(PInstance &pInst, InputPaths &inputPaths
     setInitialDuals(pInst, inputPaths, epoch);
     if (pInst->parameters_->initialDual_ == GREEDY_D) {
         for (auto & requestObj : zSolution_) {
-            requestObj->dual_ = 0.3 * requestObj->marginalCost_ + 0.7 * requestObj->penalty_;
+            requestObj->dual_ = 0.5 * requestObj->marginalCost_ + 0.5 * requestObj->penalty_;
         }
+        /*for (auto & vehicleObj : pInst->vehicles_)
+            vehicleObj->dual_ = 0;*/
         for (auto & vehicleObj : pInst->vehicles_)
-            vehicleObj->dual_ = 0;
+            MPGurobiPro_->routesToAdd_.push_back(vehicleObj->greedyRoute_);
     }
     if (availableRoutes_.size() > 0 && pInst->parameters_->routeRecycle_ &&
         (pInst->parameters_->initialDual_ == BARRIER || pInst->parameters_->initialDual_ == INITIAL_LP ||
