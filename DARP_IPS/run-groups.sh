7afd6cb9b17c46dc70dacd3e7476dfe00eeebc2f
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2025-12-02 00:41:03 -0500

    Update run-groups.sh

--- DARP_IPS/run-groups.sh
+++ DARP_IPS/run-groups.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #SBATCH --cpus-per-task=16
-#SBATCH --mem=60G
-#SBATCH --time=2:20:00
-#SBATCH --array=1-16
+#SBATCH --mem=12G
+#SBATCH --time=1:00:00
+#SBATCH --array=1-24
 #SBATCH --error=slurm-%A_%a.err
 
 # Modules and binary
@@ -90,7 +90,7 @@ G2_vehicle_counts=(2000)
 G2_algorithms=(2)
 G2_modes=(1)
 #G2_scenarios=("initial_0" "initial_1" "pruning_0" "pruning_1" "truncate_0" "truncate_1")
-G2_scenarios=("multiObj_0" "multiObj_1" "multiObj_5")
+G2_scenarios=("multiObj_5")
 #G2_inst_folder="Instances_30s_11"
 G2_inst_folder="Instances_2h-7"
 
@@ -111,7 +111,7 @@ fi
 
 
 # Register all for SELECTED_GROUPS=ALL
-ALL_GROUPS=(G11 G31 G41)
+ALL_GROUPS=(G2)
 
 # -------------------------
 # Build job list
@@ -139,7 +139,7 @@ add_group() {
       for s in "${scens_ref[@]}"; do
         for c in "${counts_ref[@]}"; do
           for inst in "${insts_ref[@]}"; do
-            jobs+=("$exe --vehicle-folder $vehicle_folder --inst-folder $inst_folder --instance-name $inst --num-vehicles $c --main-algo $a --sol-mode $m --paramfile $paramfile --scenario $s --save-scratch 1 --initial-state 1")
+            jobs+=("$exe --vehicle-folder $vehicle_folder --inst-folder $inst_folder --instance-name $inst --num-vehicles $c --main-algo $a --sol-mode $m --paramfile $paramfile --scenario $s --save-scratch 1 --initial-state 0")
           done
         done
       done
