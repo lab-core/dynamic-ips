a832fad3021a0540942cdfbbc4c95809b925efd8
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2026-01-31 00:56:47 -0500

    Update run-anytime.sh

--- DARP_IPS/run-anytime.sh
+++ DARP_IPS/run-anytime.sh
@@ -2,7 +2,7 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=36G
 #SBATCH --time=4:20:00
-#SBATCH --array=1-96
+#SBATCH --array=1-32
 #SBATCH --output=slurm-%A_%a.out
 #SBATCH --error=slurm-%A_%a.err
 
@@ -39,11 +39,11 @@ readonly BATCH_ALGOS=(6)
 readonly BATCH_MODES=(2)
 
 readonly SCENS_Rebalance=("Rebalance_no" "Rebalance_1" "Rebalance_2" "Rebalance_3" "Rebalance_4" "Rebalance_5")
-readonly SCENS_anytime=("Dynamic" "Partial" "SP_reoptimize1" "SP_reoptimize2")
+readonly SCENS_anytime=("SP_reoptimize1" "SP_reoptimize2")
 readonly SCENS_ISUD=("SP_reoptimize2")
 
 # Bundle scenario for group tests
-readonly SCENS_GROUP_TEST=("${SCENS_Rebalance[@]}")
+readonly SCENS_GROUP_TEST=("${SCENS_anytime[@]}")
 
 # -------------------------
 # GROUP DEFINITIONS
