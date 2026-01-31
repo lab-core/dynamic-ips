8852e909311a6b1c4835cacf47d963cb21f53ef9
Elahe Amiri <71190250+ElaheAmiri@users.noreply.github.com>
2026-01-31 01:01:48 -0500

    test

--- DARP_IPS/run-anytime.sh
+++ DARP_IPS/run-anytime.sh
@@ -35,15 +35,16 @@ exe="bin/realtime_DARP"
 # Shared defaults (DRY)
 # -------------------------
 readonly BATCH_PARAMFILE="AnyParameters"
-readonly BATCH_ALGOS=(6)
-readonly BATCH_MODES=(2)
+readonly BATCH_ALGOS=(2)
+readonly BATCH_MODES=(1)
 
 readonly SCENS_Rebalance=("Rebalance_no" "Rebalance_1" "Rebalance_2" "Rebalance_3" "Rebalance_4" "Rebalance_5")
 readonly SCENS_anytime=("SP_reoptimize1" "SP_reoptimize2")
 readonly SCENS_ISUD=("SP_reoptimize2")
+readonly SCENS_BATCH=("batch" "batch_1")
 
 # Bundle scenario for group tests
-readonly SCENS_GROUP_TEST=("${SCENS_anytime[@]}")
+readonly SCENS_GROUP_TEST=("${SCENS_BATCH[@]}")
 
 # -------------------------
 # GROUP DEFINITIONS
